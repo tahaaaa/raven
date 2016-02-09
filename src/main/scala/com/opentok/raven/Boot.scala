877e267d66bd4eb3811517cba5b7403773d696e9
ErnestRC <ernest@unstable.build>
2016-02-08 17:13:09 -0800

    added db connectivity check on boot

--- src/main/scala/com/opentok/raven/Boot.scala
+++ src/main/scala/com/opentok/raven/Boot.scala
@@ -6,12 +6,23 @@ import com.opentok.raven.http.AkkaApi
 import com.opentok.raven.service.{AkkaService, AkkaSystem}
 import com.typesafe.config.ConfigFactory
 
+import scala.util.{Failure, Success}
+
 
 object Boot extends FromResourcesConfig(ConfigFactory.load())
 with App with MysqlDal with AkkaSystem with AkkaService with AkkaApi {
 
+  import system.dispatcher
+
   Http().bindAndHandle(handler = routeTree, interface = HOST, port = PORT)
 
-  system.log.info(s"Raven service started and listening on $HOST:$PORT; max-retries=$MAX_RETRIES; deferrer=$DEFERRER;")
+  testDalConnectivity().andThen {
+    case s: Success[_] ⇒
+      system.log.info(s"raven service started and listening on $HOST:$PORT; max-retries=$MAX_RETRIES; deferrer=$DEFERRER;")
+    case Failure(e) ⇒
+      system.log.error(e, s"data access layer verification failed")
+      //terminate service
+      system.shutdown()
+  }
 
 }
--- src/main/scala/com/opentok/raven/dal/Dal.scala
+++ src/main/scala/com/opentok/raven/dal/Dal.scala
@@ -1,8 +1,13 @@
 package com.opentok.raven.dal
 
+import com.opentok.raven.RavenConfig
 import com.opentok.raven.dal.components.{EmailRequestDao, EmailRequestSlickDao}
 import slick.driver.JdbcProfile
-import slick.jdbc.JdbcBackend
+import slick.jdbc.{SetParameter, SQLActionBuilder, JdbcBackend}
+
+import scala.concurrent.duration._
+import scala.concurrent.{Await, Future}
+import scala.util.Try
 
 /**
  * Jdbc data access interface
@@ -18,7 +23,16 @@ trait Dal {
  * it loads the database configuration and instantiates the ``EmailRequestDao``.
  */
 trait MysqlDal extends Dal {
+  this: RavenConfig ⇒
+
   implicit val driver: JdbcProfile = slick.driver.MySQLDriver
   implicit val db: JdbcBackend#Database = driver.backend.Database.forConfig("raven.database")
   val emailRequestDao: EmailRequestDao = new EmailRequestSlickDao()
+
+  def testDalConnectivity(): Future[Int] = Future.fromTry(Try {
+    import driver.api._
+    val builder = SQLActionBuilder.apply(Seq(DB_CHECK.replace(";", "")), SetParameter.SetUnit)
+    //force timeout
+    Await.result(db.run(builder.as[Int].head), 5.seconds)
+  })
 }
